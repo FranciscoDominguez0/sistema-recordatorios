babb8a306ac3a8f97ca05c5544ccbda935e27a41
Francisco <dominguezf225@gmail.com>
2026-03-09 13:58:12 -0500

    conexion inicial backend mysql

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,1 +1,27 @@
+import express from "express";
+import pool from "./config/database.js";
 
+const app = express();
+
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  try {
+    const [rows] = await pool.query("SELECT NOW() as time");
+
+    res.json({
+      message: "Servidor funcionando",
+      database_time: rows[0].time
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: error.message
+    });
+  }
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
